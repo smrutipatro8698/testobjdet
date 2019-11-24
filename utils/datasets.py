679866bae910591c96fb13d0d5413cf3c4408fcb
smrutipatro <pratimapatro11@gmail.com>
2019-11-24 21:38:11 +0530

    Fixed bugs

--- utils/datasets.py
+++ utils/datasets.py
@@ -26,7 +26,7 @@ def pad_to_square(img, pad_value):
 
 
 def resize(image, size):
-    image = transforms.RandomResizedCrop(416)
+    image = transfroms.Compose([transforms.RandomResizedCrop(416),transforms.ToTensor()])
     print(type(image))
     #image = F.interpolate(image.unsqueeze(0), size=size, mode="nearest").squeeze(0)
     return image
