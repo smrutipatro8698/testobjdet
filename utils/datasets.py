7dc077bf3abd0d55398323daede1e93902c73e2e
smrutipatro <pratimapatro11@gmail.com>
2019-11-24 23:27:26 +0530

    Fixed bugs

--- utils/datasets.py
+++ utils/datasets.py
@@ -151,6 +151,7 @@ class ListDataset(Dataset):
             self.img_size = random.choice(range(self.min_size, self.max_size + 1, 32))
         # Resize images to input shape
         imgs = torch.stack([resize(img, self.img_size) for img in imgs])
+        print("After stacking",imgs.size())
         self.batch_count += 1
         return paths, imgs, targets
 
